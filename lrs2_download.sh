16913311b5788fc9913e44ea05b36e4df1b24561
Nicholas Cheng <nche0017@student.monash.edu>
2020-12-14 11:50:45 +0800

    Add LRS2 download script

---
+++ lrs2_download.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+if ! command -v wget &> /dev/null
+then
+    echo "wget is not installed or in \$PATH."
+    exit 1
+fi
+
+if [ -z "$LRS2_USERNAME" ] || [ -z "$LRS2_PASSWORD" ]
+then
+    echo "LRS2_USERNAME or LRS2_PASSWORD environment variable is not set. Please set them to continue downloading."
+    exit 1
+fi
+echo "Starting LRS2 dataset download to ./lrs2/"
+
+echo "Creating ./lrs2/"
+mkdir -p ./lrs2/
+
+echo "Changing directory to ./lrs2/"
+cd ./lrs2/
+
+echo "Downloading dataset parts sequentially"
+for letter in {a..e}
+do
+    echo "Downloading parta$letter"
+    wget --user $LRS2_USERNAME --password $LRS2_PASSWORD --continue "https://www.robots.ox.ac.uk/~vgg/data/lip_reading/data2/lrs2_v1_parta$letter"
+done
+
+echo "Downloading filelists"
+for file in 'pretrain.txt' 'train.txt' 'val.txt' 'test.txt'
+do
+    echo "Downloading filelist $file"
+    wget --user $LRS2_USERNAME --password $LRS2_PASSWORD --continue "https://www.robots.ox.ac.uk/~vgg/data/lip_reading/data2/$file"
+done
+
+echo "To combine all the parts into one .tar file, run the following command"
+echo 'cat lrs2_v1_parta* > lrs2_v1.tar'
