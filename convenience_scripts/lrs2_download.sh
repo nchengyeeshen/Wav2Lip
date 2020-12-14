d9ac661f3eb4a8d9a17015c1829a9bfff7198119
Nicholas Cheng <nche0017@student.monash.edu>
2020-12-14 12:12:54 +0800

    Use single-quotes if interpolation not needed

--- convenience_scripts/lrs2_download.sh
+++ convenience_scripts/lrs2_download.sh
@@ -2,36 +2,36 @@
 
 if ! command -v wget &> /dev/null
 then
-    echo "wget is not installed or in \$PATH."
+    echo 'wget is not installed or in $PATH.'
     exit 1
 fi
 
 if [ -z "$LRS2_USERNAME" ] || [ -z "$LRS2_PASSWORD" ]
 then
-    echo "LRS2_USERNAME or LRS2_PASSWORD environment variable is not set. Please set them to continue downloading."
+    echo 'LRS2_USERNAME or LRS2_PASSWORD environment variable is not set. Please set them to continue downloading.'
     exit 1
 fi
-echo "Starting LRS2 dataset download to ./lrs2/"
+echo 'Starting LRS2 dataset download to ./lrs2/'
 
-echo "Creating ./lrs2/"
+echo 'Creating ./lrs2/'
 mkdir -p ./lrs2/
 
-echo "Changing directory to ./lrs2/"
+echo 'Changing directory to ./lrs2/'
 cd ./lrs2/ || exit 1
 
-echo "Downloading dataset parts sequentially"
+echo 'Downloading dataset parts sequentially'
 for letter in {a..e}
 do
     echo "Downloading parta$letter"
     wget --user "$LRS2_USERNAME" --password "$LRS2_PASSWORD" --continue "https://www.robots.ox.ac.uk/~vgg/data/lip_reading/data2/lrs2_v1_parta$letter"
 done
 
-echo "Downloading filelists"
+echo 'Downloading filelists'
 for file in 'pretrain.txt' 'train.txt' 'val.txt' 'test.txt'
 do
     echo "Downloading filelist $file"
     wget --user "$LRS2_USERNAME" --password "$LRS2_PASSWORD" --continue "https://www.robots.ox.ac.uk/~vgg/data/lip_reading/data2/$file"
 done
 
-echo "To combine all the parts into one .tar file, run the following command"
+echo 'To combine all the parts into one .tar file, run the following command'
 echo 'cat lrs2_v1_parta* > lrs2_v1.tar'
--- convenience_scripts/virtual_env_setup.sh
+++ convenience_scripts/virtual_env_setup.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-echo "Installing python3-venv"
+echo 'Installing python3-venv'
 sudo apt-get update
 sudo apt-get install -y python3-venv
 
-echo "Creating virtual environment in ./venv/"
+echo 'Creating virtual environment in ./venv/'
 python3 -m venv ./venv/
 
-echo "Activating virtual environment"
+echo 'Activating virtual environment'
 source ./venv/bin/activate
 
-echo "Updating pip before installing Python dependencies"
+echo 'Updating pip before installing Python dependencies'
 pip install --upgrade pip
 
-echo "Installing Python dependencies"
+echo 'Installing Python dependencies'
 pip install -r ./requirements.txt
